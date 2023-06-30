d182300cb0ee01504c382c10763a689e90bab74f
CharlyAnne <icharlieanne@gmail.com>
2023-06-30 02:39:10 +0100

    task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,26 +1,25 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw TypeError('Invalid input');
-    }
-    if (
+    /* if (
       this.constructor !== Building &&
-      this.evacuationWarningMesagge ===
-        Building.prototype.evacuationWarningMessage
+      typeof this.evacuationWarningMesagge !== 'function'
     ) {
       throw new Error(
         'Class extending Building must override evacuationWarningMessage'
       );
-    }
+    } */
     this._sqft = sqft;
-  }
-
-  evacuationWarningMessage() {
-    // eslint-disable-line
   }
 
   get sqft() {
     return this._sqft;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  evacuationWarningMessage() {
+    throw new Error(
+      'Class extending Building must override evacuationWarningMessage'
+    );
   }
 
   set sqft(sqft) {
