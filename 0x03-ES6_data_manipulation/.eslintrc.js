9017ebf90bf6fa84b6eece2043b33750543a114a
CharlyAnne <icharlieanne@gmail.com>
2023-07-04 09:51:07 +0100

    eslint rules

--- 0x03-ES6_data_manipulation/.eslintrc.js
+++ 0x03-ES6_data_manipulation/.eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     'operator-linebreak': ['error', 'before'],
     'next-line no-unused-vars': 'off',
     'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+    'prefer-arrow-callback': 'off',
   },
   overrides: [
     {
