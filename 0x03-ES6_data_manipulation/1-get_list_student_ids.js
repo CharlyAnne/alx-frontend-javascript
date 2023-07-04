dbceff7ffa9df6a199bf8fc3e93ed1116f996d2e
CharlyAnne <icharlieanne@gmail.com>
2023-07-04 09:58:11 +0100

    TASK 1:  mapping

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line consistent-return
+export default function getListStudentIds() {
+  // eslint-disable-next-line no-undef
+  if (!Array.isArray(arr)) {
+    return [];
+  }
+  // eslint-disable-next-line no-undef
+  const ids = arr.map((student) => student.ids);
+  return ids;
+}
