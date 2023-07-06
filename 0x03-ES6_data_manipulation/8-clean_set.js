416532ac95ab427e098bae6264024bf85159ee10
CharlyAnne <icharlieanne@gmail.com>
2023-07-06 20:24:35 +0100

    task 8: Set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,13 +2,14 @@ export default function cleanSet(set, startString) {
   if (!startString || !startString.length || typeof startString !== 'string')
     // eslint-disable-next-line nonblock-statement-body-position, curly
     return '';
+
   let arr = '';
 
-  for (const element of set) {
+  set.forEach((element) => {
     if (typeof element === 'string' && element.startsWith(startString)) {
       arr += `${element.slice(startString.length)}-`;
     }
-  }
+  });
 
   return arr.slice(0, arr.length - 1);
 }
