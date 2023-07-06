5a84ad78bad9cc7c05ea860117504ffc2ee30b8c
CharlyAnne <icharlieanne@gmail.com>
2023-07-06 20:38:37 +0100

    task 10: more map DS

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(items) {
+  if (!(items instanceof Map)) {
+    throw new Error('cannot process');
+  }
+  for (const [key, value] of items) {
+    if (value === 1) {
+      items.set(key, 100);
+    }
+  }
+  return items;
+}
